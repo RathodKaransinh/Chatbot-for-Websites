2a71b2d8d486fa92411016267f0ce14a0a51a15e
rathod_karansinh <rathodkaransinh125@gmail.com>
2023-11-12 22:43:18 -0500

    first commit

---
+++ chat1/views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from nltk.tokenize import word_tokenize
+
+def find_list():
+    f = open("E:/Django projects/all.txt",'r')
+    lines = f.readlines()
+    urls_list = []
+    urls_keyword = []
+    i=1
+    for line in lines:
+        # print(line,i)
+        i += 1
+        line = line.replace('\n','')
+        line = line.split(',')
+        urls_list.append(line[0])
+        urls_keyword.append(line[1])
+    f.close()
+    # f = open("E:/Django projects/all1.txt",'r')
+    # lines = f.readlines()
+    # urls_list1 = []
+    # urls_keyword1 = []
+    # for line in lines:
+    #     line = line.replace('\n','')
+    #     line = line.split(',')
+    #     urls_list1.append(line[0])
+    #     urls_keyword1.append(line[1])
+    # f.close()
+    return urls_list, urls_keyword
+
+def find_link(query):
+    tokens = query.split(' ')
+    urls_list , urls_keyword = find_list()
+    j = 0
+    count = []
+    
+    for i in urls_keyword:
+        count.append(0)
+        for word in tokens:
+            if word in i:
+                count[j] = count[j] + 1
+        j = j+1
+    
+    j, ans = 0, 0
+    max = 0
+    for cnt in count:
+        if(max<cnt):
+            max = cnt
+            ans = j
+        j = j+1
+    
+    # j = 0  
+    # for i in urls_keyword1:
+    #     for word in tokens:
+    #         if word in i:
+    #             return urls_list1[j]
+    #     j = j+1
+
+    if (max==0):
+        return "Nothing found"
+    else:
+        return urls_list[ans]
+
+def index(request):
+    return render(request, "chat1/index3.html")
+
+def specific(request):
+    # return HttpResponse('This is specific')
+    return render(request, "chat1/index3.html")
+
+def getResponse(request):
+    userMessage = request.GET.get('userMessage')
+    # print(userMessage+'getresponse')
+    reply = find_link(userMessage.lower())
+    # userMessage2 = find_link1(userMessage.lower())
+    return HttpResponse(reply)
+
+
